0702ecde2093009151a64364489e280fa70354d5
jayanth920 <jayanthgajula92004@gmail.com>
2022-12-25 03:44:51 -0600

    added logo, register form code

--- src/pages/RegisterPage.js
+++ src/pages/RegisterPage.js
@@ -1,8 +1,55 @@
 import React from 'react'
+import styled from 'styled-components'
+import { Link } from'react-router-dom'
+
 
 function RegisterPage() {
-  return (
-    <div>RegisterPage</div>
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    alert("form")
+  }
+
+      return (
+    <>
+    <form onSubmit={(e)=>handleSubmit(e)}>
+        <div className="brand">
+          <img src="./assets/logo.png" alt="Logo" />
+          <h1>Chatify</h1>
+        </div>
+        <input 
+        type="text" 
+        palceholder="UserName" 
+        name="username"
+        onChange={(e)=>handleSubmit(e)}/>
+
+        <input 
+        type="text" 
+        palceholder="UserName" 
+        name="username"
+        onChange={(e)=>handleSubmit(e)}/>
+
+        <input 
+        type="email" 
+        palceholder="Email" 
+        name="email" 
+        onChange={(e)=>handleSubmit(e)}/>
+
+        <input 
+        type="password" 
+        palceholder="Password" 
+        name="password"
+        onChange={(e)=>handleSubmit(e)}/>
+
+        <input 
+        type="password" 
+        palceholder="Confirm Password" 
+        name="confirmPassword"
+        onChange={(e)=>handleSubmit(e)}/>
+        <button type="submit">Create User</button>
+        <span>Already have an account ?<Link to="/login">Login</Link></span>
+    </form>
+    </>
   )
 }
 
