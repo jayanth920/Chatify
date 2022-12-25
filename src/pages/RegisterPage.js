1df44c9249504fae0a876e9fe3559b41150a4b75
jayanth920 <jayanthgajula92004@gmail.com>
2022-12-25 04:24:31 -0600

    made changes with scss,register form

--- src/pages/RegisterPage.js
+++ src/pages/RegisterPage.js
@@ -1,7 +1,8 @@
-import React from 'react'
-import styled from 'styled-components'
-import { Link } from'react-router-dom'
-
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import styled from "styled-components";
+import { useNavigate, Link } from "react-router-dom";
+import Logo from "../assets/logo.png"
 
 function RegisterPage() {
 
@@ -10,47 +11,117 @@ function RegisterPage() {
     alert("form")
   }
 
-      return (
+  
+
+  return (
     <>
-    <form onSubmit={(e)=>handleSubmit(e)}>
-        <div className="brand">
-          <img src="./assets/logo.png" alt="Logo" />
-          <h1>Chatify</h1>
-        </div>
-        <input 
-        type="text" 
-        palceholder="UserName" 
-        name="username"
-        onChange={(e)=>handleSubmit(e)}/>
-
-        <input 
-        type="text" 
-        palceholder="UserName" 
-        name="username"
-        onChange={(e)=>handleSubmit(e)}/>
-
-        <input 
-        type="email" 
-        palceholder="Email" 
-        name="email" 
-        onChange={(e)=>handleSubmit(e)}/>
-
-        <input 
-        type="password" 
-        palceholder="Password" 
-        name="password"
-        onChange={(e)=>handleSubmit(e)}/>
-
-        <input 
-        type="password" 
-        palceholder="Confirm Password" 
-        name="confirmPassword"
-        onChange={(e)=>handleSubmit(e)}/>
-        <button type="submit">Create User</button>
-        <span>Already have an account ?<Link to="/login">Login</Link></span>
-    </form>
+      <FormContainer>
+        <form action="" onSubmit={(event) => handleSubmit(event)}>
+          <div className="brand">
+            <img src={Logo} alt="logo" />
+            <h1>snappy</h1>
+          </div>
+          <input
+            type="text"
+            placeholder="Username"
+            name="username"
+            onChange={(e) => handleChange(e)}
+          />
+          <input
+            type="email"
+            placeholder="Email"
+            name="email"
+            onChange={(e) => handleChange(e)}
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            name="password"
+            onChange={(e) => handleChange(e)}
+          />
+          <input
+            type="password"
+            placeholder="Confirm Password"
+            name="confirmPassword"
+            onChange={(e) => handleChange(e)}
+          />
+          <button type="submit">Create User</button>
+          <span>
+            Already have an account ? <Link to="/login">Login.</Link>
+          </span>
+        </form>
+      </FormContainer>
+      <ToastContainer />
     </>
-  )
+  );
 }
+
+const FormContainer = styled.div`
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 1rem;
+  align-items: center;
+  background-color: #131324;
+  .brand {
+    display: flex;
+    align-items: center;
+    gap: 1rem;
+    justify-content: center;
+    img {
+      height: 5rem;
+    }
+    h1 {
+      color: white;
+      text-transform: uppercase;
+    }
+  }
+  form {
+    display: flex;
+    flex-direction: column;
+    gap: 2rem;
+    background-color: #00000076;
+    border-radius: 2rem;
+    padding: 3rem 5rem;
+  }
+  input {
+    background-color: transparent;
+    padding: 1rem;
+    border: 0.1rem solid #4e0eff;
+    border-radius: 0.4rem;
+    color: white;
+    width: 100%;
+    font-size: 1rem;
+    &:focus {
+      border: 0.1rem solid #997af0;
+      outline: none;
+    }
+  }
+  button {
+    background-color: #4e0eff;
+    color: white;
+    padding: 1rem 2rem;
+    border: none;
+    font-weight: bold;
+    cursor: pointer;
+    border-radius: 0.4rem;
+    font-size: 1rem;
+    text-transform: uppercase;
+    &:hover {
+      background-color: #4e0eff;
+    }
+  }
+  span {
+    color: white;
+    text-transform: uppercase;
+    a {
+      color: #4e0eff;
+      text-decoration: none;
+      font-weight: bold;
+    }
+  }
+`;
 
 export default RegisterPage
