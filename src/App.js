4d2fb0590192af04ecdc99fb831560333381faa6
jayanth920 <jayanthgajula92004@gmail.com>
2022-12-25 02:48:09 -0600

    added all 3 pages in app.js for now

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import RegisterPage from './pages/RegisterPage'
-
+import LoginPage from './pages/LoginPage'
+import ChatPage from './pages/ChatPage'
 export default function App() {
   return (
     <BrowserRouter>
     <Routes>
     <Route path='/register' element={<RegisterPage/>}/>
+    <Route path='/login' element={<LoginPage/>}/>
+    <Route path='/' element={<ChatPage/>}/>
     </Routes>
     </BrowserRouter>
   )
