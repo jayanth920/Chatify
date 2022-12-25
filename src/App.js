4030585755cb0c7529d5ea0ac23853b1d3a7b72d
jayanth920 <jayanthgajula92004@gmail.com>
2022-12-25 02:32:05 -0600

    added assets folder and added basic register route code in app.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import RegisterPage from './pages/RegisterPage'
 
 export default function App() {
   return (
-    <div>App</div>
+    <BrowserRouter>
+    <Routes>
+    <Route path='/register' element={<RegisterPage/>}/>
+    </Routes>
+    </BrowserRouter>
   )
 }
