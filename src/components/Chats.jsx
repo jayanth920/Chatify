2673b68150a9133a6cc1e04a12ae55ba10e8f4c0
jayanth920 <jayanthgajula92004@gmail.com>
2022-12-26 11:25:12 -0600

    made chats

--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -10,6 +10,21 @@ const Chats = () => {
                 <p>Hello</p>
             </div>
         </div>
+        <div className="userChat">
+            <img src="https://thumbs.dreamstime.com/b/autumn-fall-nature-scene-autumnal-park-beautiful-77869343.jpg" alt="" />
+            <div className="userChatInfo">
+                <span>James</span>
+                <p>Hello</p>
+            </div>
+        </div>
+        <div className="userChat">
+            <img src="https://thumbs.dreamstime.com/b/autumn-fall-nature-scene-autumnal-park-beautiful-77869343.jpg" alt="" />
+            <div className="userChatInfo">
+                <span>James</span>
+                <p>Hello</p>
+            </div>
+        </div>
+
     </div>
   )
 }
