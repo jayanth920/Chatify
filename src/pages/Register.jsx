6ccf2081c5fe4e161403aab29031c75f54183ec7
jayanth920 <jayanthgajula92004@gmail.com>
2022-12-26 14:50:48 -0600

    register jsx made at last , still updates might remain

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,21 +1,74 @@
-import React from 'react'
-import AddImage from "../img/addImage.png"
+import React, { useState } from "react";
+import AddImage from "../img/addImage.png";
+import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
+import { auth, db, storage } from "../firebase";
+import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
+import { doc, setDoc } from "firebase/firestore";
+
+
 const Register = () => {
-  return (
+  const [err, setErr] = useState(false);
+
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const displayName = e.target[0].value;
+    const email = e.target[1].value;
+    const password = e.target[2].value;
+    const file = e.target[3].files[0];
+
+    try {
+      //Create user
+      const res = await createUserWithEmailAndPassword(auth, email, password);
+
+      //Create a unique image name
+      const date = new Date().getTime();
+      const storageRef = ref(storage, `${displayName + date}`);
+
+      await uploadBytesResumable(storageRef, file).then(() => {
+        getDownloadURL(storageRef).then(async (downloadURL) => {
+          try {
+            //Update profile
+            await updateProfile(res.user, {
+              displayName,
+              photoURL: downloadURL,
+            });
+            //create user on firestore
+            await setDoc(doc(db, "users", res.user.uid), {
+              uid: res.user.uid,
+              displayName,
+              email,
+              photoURL: downloadURL,
+            });
+
+            //create empty user chats on firestore
+            await setDoc(doc(db, "userChats", res.user.uid), {});
+          } catch (err) {
+            console.log(err);
+            setErr(true);
+          }
+        });
+      });
+    } catch (err) {
+      setErr(true);
+    }
+  };
+    return (
     <div className="formContainer">
       <div className="formWrapper">
         <span className="logo">CHATIFY</span>
         <span className="title">Register</span>
-        <form>
+        <form onSubmit={handleSubmit}>
           <input required type="text" placeholder="Name" />
           <input required type="email" placeholder="Email" />
           <input required type="password" placeholder="Password" />
           <input style={{display:"none"}}type="file"id="file"/> 
-          <label htmlFor="">
+          <label htmlFor="file">
            <img src={AddImage} alt="AddImage"></img>
            <label>Add your Profile Picture</label>
           </label>
           <button>Sign up</button>
+          {err && <span>Something went wrong!</span>}
         </form>
         <p> Do you have an account? Login</p>
       </div>
