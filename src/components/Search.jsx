3f6171c24e4a17da2354327eb5754cc2c0ac33d7
jayanth920 <jayanthgajula92004@gmail.com>
2022-12-25 20:18:23 -0600

    searchbar update

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
 
+
+
 const Search = () => {
   return (
-    <div>Search</div>
+    <div className='search'>
+        <div className="searchForm">
+            <input type="text" />
+        </div>
+        <div className="userChat">
+            <div className="userChatInfo">
+                <span>
+                    James
+                </span>
+            </div>
+        </div>
+    </div>
   )
 }
 
