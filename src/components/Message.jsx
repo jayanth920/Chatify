d355a275a9f1c755cee56f14f42b7cacb02eff64
jayanth920 <jayanthgajula92004@gmail.com>
2022-12-26 11:25:34 -0600

    made message

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -2,7 +2,16 @@ import React from 'react'
 
 const Message = () => {
   return (
-    <div>Message</div>
+    <div className='message owner'>
+        <div className="messageInfo">
+        <img src="https://thumbs.dreamstime.com/b/autumn-fall-nature-scene-autumnal-park-beautiful-77869343.jpg" alt="" />
+        <span> just now </span>
+        </div>
+        <div className="messageContent">
+            <p>Hello</p>
+            <img src="https://thumbs.dreamstime.com/b/autumn-fall-nature-scene-autumnal-park-beautiful-77869343.jpg" alt="" />
+        </div>
+    </div>
   )
 }
 
