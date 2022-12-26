80e9b8e5eca18a53ce89e2bf4cfe79a8ee166551
jayanth920 <jayanthgajula92004@gmail.com>
2022-12-25 20:23:56 -0600

    updated search bar for font, and sample img link for now

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -9,6 +9,7 @@ const Search = () => {
             <input type="text" />
         </div>
         <div className="userChat">
+            <img src="https://thumbs.dreamstime.com/b/autumn-fall-nature-scene-autumnal-park-beautiful-77869343.jpg" alt="" />
             <div className="userChatInfo">
                 <span>
                     James
