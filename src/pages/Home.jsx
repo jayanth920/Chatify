72dba0c136156507629738eaf5921cc8bc4fb4d4
jayanth920 <jayanthgajula92004@gmail.com>
2022-12-25 20:18:47 -0600

    home js update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Sidebar from '../components/Sidebar'
 import Chat from '../components/Chat'
-
+import "../App.scss"
 function Home() {
   return (
     <div className="home">
