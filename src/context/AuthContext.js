ceeb32f627d55250e24d6648ad3a40a834178783
jayanth920 <jayanthgajula92004@gmail.com>
2022-12-27 15:16:57 -0600

    auth context complete

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,21 +1,26 @@
-import { onAuthStateChanged } from 'firebase/auth';
-import { createContext, useEffect, useState } from 'react';
-import {auth} from "../firebase"
-export const AuthContext = createContext()
+import { createContext, useEffect, useState } from "react";
+import { auth } from "../firebase";
+import { onAuthStateChanged } from "firebase/auth";
 
-export const AuthContextProvider = ({children})=>{
-    const [currentUser,setCurrentUser]=useState({})
+export const AuthContext = createContext();
 
-    useEffect(()=> {
-        onAuthStateChanged(auth,(user)=>{
-            setCurrentUser(user)
-            console.log(user)
-        })
-    }, []); 
-return(
-    <AuthContext.Provider value={{currentUser}}>
-        {children}
+export const AuthContextProvider = ({ children }) => {
+  const [currentUser, setCurrentUser] = useState({});
+
+  useEffect(() => {
+    const unsub = onAuthStateChanged(auth, (user) => {
+      setCurrentUser(user);
+      console.log(user);
+    });
+
+    return () => {
+      unsub();
+    };
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ currentUser }}>
+      {children}
     </AuthContext.Provider>
-)
-
-}
+  );
+};
