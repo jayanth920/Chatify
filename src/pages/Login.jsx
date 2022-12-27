5db07d2336477e0f0947e3671c90f19501db2714
jayanth920 <jayanthgajula92004@gmail.com>
2022-12-27 15:19:02 -0600

    made total, used await , minor changes

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,20 +1,39 @@
-import React from 'react'
+import React, { useState } from "react";
+import { useNavigate, Link } from "react-router-dom";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../firebase";
 
 const Login = () => {
+  const [err, setErr] = useState(false);
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const email = e.target[0].value;
+    const password = e.target[1].value;
+
+    try {
+      await signInWithEmailAndPassword(auth, email, password);
+      navigate("/")
+    } catch (err) {
+      setErr(true);
+    }
+  };
   return (
     <div className="formContainer">
       <div className="formWrapper">
         <span className="logo">CHATIFY</span>
         <span className="title">Login</span>
-        <form>
-          <input required type="email" placeholder="Email" />
-          <input required type="password" placeholder="Password" />
-          <button>Log In</button>
+        <form onSubmit={handleSubmit}>
+          <input type="email" placeholder="email" />
+          <input type="password" placeholder="password" />
+          <button>Sign in</button>
+          {err && <span>Something went wrong</span>}
         </form>
-        <p> Dont have an account? Sign Up!</p>
+        <p>You don't have an account? <Link to="/register">Register</Link></p>
       </div>
     </div>
   );
-}
+};
 
 export default Login;
