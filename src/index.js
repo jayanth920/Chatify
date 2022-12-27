a6555ccaa4417afada7e28aa988ab5701a368122
jayanth920 <jayanthgajula92004@gmail.com>
2022-12-26 18:06:18 -0600

    react 17 index still remains, will change after commit

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import { AuthContextProvider } from "./context/AuthContext";
 ReactDOM.render(
+  <AuthContextProvider>
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
+  </AuthContextProvider>,
   document.getElementById("root")
 );
