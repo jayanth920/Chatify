e95bb250b580b99c6fa1227e7fb2020dacbafe3e
jayanth920 <jayanthgajula92004@gmail.com>
2022-12-26 18:07:28 -0600

    at last react 18 working with index,my gggggoooodd

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import App from "./App";
 import { AuthContextProvider } from "./context/AuthContext";
-ReactDOM.render(
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
   <AuthContextProvider>
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-  </AuthContextProvider>,
-  document.getElementById("root")
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+  </AuthContextProvider>
 );
