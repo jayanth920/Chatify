888c881c1ecd114b2d125bb89f1917c638c0a79a
jayanth920 <jayanthgajula92004@gmail.com>
2022-12-26 22:57:17 -0600

    id modif

--- src/firebase.js
+++ src/firebase.js
@@ -3,15 +3,16 @@ import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 import { getFirestore } from "firebase/firestore";
 
-
 const firebaseConfig = {
-    apiKey: "AIzaSyDcNSPQidDGJFKezk6YsbODRohHZg11MpQ",
-    authDomain: "chatify-61055.firebaseapp.com",
-    projectId: "chatify-61055",
-    storageBucket: "chatify-61055.appspot.com",
-    messagingSenderId: "519907436309",
-    appId: "1:519907436309:web:d63a183c80c655c0ad6b1c"
-  };
+  apiKey: "AIzaSyDcNSPQidDGJFKezk6YsbODRohHZg11MpQ",
+  authDomain: "chatify-61055.firebaseapp.com",
+  databaseURL: "https://chatify-61055-default-rtdb.firebaseio.com",
+  projectId: "chatify-61055",
+  storageBucket: "chatify-61055.appspot.com",
+  messagingSenderId: "519907436309",
+  appId: "1:519907436309:web:d63a183c80c655c0ad6b1c"
+};
+
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth();
