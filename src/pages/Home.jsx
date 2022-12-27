6270d02da939d38ca7c51bf16f0a73a53b2e1033
jayanth920 <jayanthgajula92004@gmail.com>
2022-12-27 15:18:34 -0600

    scss removed , func to const

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import Sidebar from '../components/Sidebar'
 import Chat from '../components/Chat'
-import "../App.scss"
-function Home() {
+
+const Home = () => {
   return (
-    <div className="home">
-        <div className="container">
-            <Sidebar/>
-            <Chat/>
-        </div>
+    <div className='home'>
+      <div className="container">
+        <Sidebar/>
+        <Chat/>
+      </div>
     </div>
   )
 }
