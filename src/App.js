9c4912233185a245fcc95916fe09514c98cab560
jayanth920 <jayanthgajula92004@gmail.com>
2022-12-26 18:05:57 -0600

    auth context

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import React from "react";
+import React, { useContext } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Home from "./pages/Home";
 import "./App.scss"
 import Register from "./pages/Register";
+import { AuthContext } from "./context/AuthContext";
 
 export default function App() {
+
+  const {currentUser} = useContext(AuthContext)
+  console.log(currentUser)
   return (
     <BrowserRouter>
       <Routes>
