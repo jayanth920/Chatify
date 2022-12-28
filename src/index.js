46820dee0668c87c4521a7255d49753986b058fa
jayanth920 <jayanthgajula92004@gmail.com>
2022-12-27 20:28:16 -0600

    downgraded to react 17 due to packages not working

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 import App from "./App";
 import { AuthContextProvider } from "./context/AuthContext";
 import { ChatContextProvider } from "./context/ChatContext";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
+ReactDOM.render(
   <AuthContextProvider>
     <ChatContextProvider>
       <React.StrictMode>
         <App />
       </React.StrictMode>
     </ChatContextProvider>
-  </AuthContextProvider>
+  </AuthContextProvider>,
+  document.getElementById("root")
 );
