fbe86bf361091441320357ce3a2a31124741106f
jayanth920 <jayanthgajula92004@gmail.com>
2022-12-27 20:52:17 -0600

    shifted app js

--- src/App.js
+++ src/App.js
@@ -1,40 +1,13 @@
-import Home from "./pages/Home";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
-import "./App.scss";
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import { useContext } from "react";
-import { AuthContext } from "./context/AuthContext";
+import Main from "./Main";
+import React from 'react'
+import AnimCursor from "./Extras/AnimCursor";
 
-function App() {
-  const { currentUser } = useContext(AuthContext);
-
-  const ProtectedRoute = ({ children }) => {
-    if (!currentUser) {
-      return <Navigate to="/login" />;
-    }
-
-    return children
-  };
-
+ function App () {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/">
-          <Route
-            index
-            element={
-              <ProtectedRoute>
-                <Home />
-              </ProtectedRoute>
-            }
-          />
-          <Route path="login" element={<Login />} />
-          <Route path="register" element={<Register />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
-  );
+    <>
+    <AnimCursor />
+    <Main />
+    </>
+  )
 }
-
-export default App;
+export default App
