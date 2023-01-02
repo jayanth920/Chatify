dac8613b325e97cc9ec5fd027abe6edc4f79cb64
jayanth920 <jayanthgajula92004@gmail.com>
2023-01-02 15:14:46 -0600

    updated import

--- src/components/Messages.jsx
+++ src/components/Messages.jsx
@@ -1,6 +1,6 @@
 import { doc, onSnapshot } from "firebase/firestore";
 import React, { useContext, useEffect, useState } from "react";
-import { ChatContext } from "../context/ChatContext";
+import { ChatContext } from "../render/ChatRender";
 import { db } from "../firebase";
 import Message from "./Message";
 
