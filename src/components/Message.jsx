0511accd2ab0080e225bdcebb44699b5f6d64f19
jayanth920 <jayanthgajula92004@gmail.com>
2023-01-02 15:14:11 -0600

    context api

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,6 +1,6 @@
 import React, { useContext, useEffect, useRef } from "react";
-import { AuthContext } from "../context/AuthContext";
-import { ChatContext } from "../context/ChatContext";
+import { AuthContext } from "../render/Authorization";
+import { ChatContext } from "../render/ChatRender";
 
 const Message = ({ message }) => {
   const { currentUser } = useContext(AuthContext);
