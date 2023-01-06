7543266059c65ecb2a5360ac3d429209c0068676
jayanth920 <jayanthgajula92004@gmail.com>
2023-01-05 22:57:04 -0600

    auth context ,update imports

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import React, { useContext } from 'react'
 import {signOut} from "firebase/auth"
 import { auth } from '../firebase'
-import { AuthContext } from '../render/Authorization'
+import { AuthContext } from '../render/AuthContext'
 
 const Navbar = () => {
   const {currentUser} = useContext(AuthContext)
