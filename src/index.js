1a08c4625200db03b2c5c0f4b8b48d3f0b007681
jayanth920 <jayanthgajula92004@gmail.com>
2023-01-05 22:56:18 -0600

    auth context , chat context ,update imports

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import { AuthContextProvider } from "./render/Authorization";
-import { ChatContextProvider } from "./render/ChatRender";
+import { AuthContextProvider } from "./render/AuthContext";
+import { ChatContextProvider } from "./render/ChatContext";
 
 ReactDOM.render(
   <AuthContextProvider>
